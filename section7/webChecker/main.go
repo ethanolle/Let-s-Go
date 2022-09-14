1a5e39cd7af73649bc6db4b13cac4028a1752c2b
ethan.sayagh ethan.sayagh <ethan.sayagh@gmail.com>
2022-09-14 19:09:38 +0200

    finished the course

--- section7/webChecker/main.go
+++ section7/webChecker/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -20,8 +21,12 @@ func main() {
 		go checkLink(link, c)
 	}
 
-	for i:=0;i<len(links);i++ {
-		fmt.Println(<-c)
+	for l := range c {
+ 
+		go func(link string){
+			time.Sleep(5 *time.Second)
+			checkLink(link, c)
+		}(l)
 	}
 }
 
@@ -29,9 +34,9 @@ func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link,"is down")
-		c <- "Might Be Down lol"
+		c <- link
 		return
 	}
 	fmt.Println(link,"is up")
-	c <- "It's up lol"
+	c <- link
 }
